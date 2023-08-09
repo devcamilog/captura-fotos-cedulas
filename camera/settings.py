6b94f6eec669305820e512d560780b453d481bb4
devcamilog <kamy.626@hotmail.com>
2023-08-09 13:17:10 -0500

    SETTINGS UPDATE

--- camera/settings.py
+++ camera/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'templates', 'Appcamera', 'static')]
+STATICFILES_DIRS = ['CAMERA_PROJECT/camera/Templates/Appcamera/static']
 
 MEDIA_ROOT = '\\\\172.20.0.112\\Public\\FotosPV\\'
 MEDIA_URL = '/media/'
