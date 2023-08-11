82d323e97f993e7d9f4d093ea803a39491a1d116
devcamilog <kamy.626@hotmail.com>
2023-08-11 19:49:50 +0000

    subir archivos que me faltaban

--- camera/wsgi.py
+++ camera/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camera.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camera.production')
 
 application = get_wsgi_application()
